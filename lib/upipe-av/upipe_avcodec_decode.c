fddf7b35826be0ce2ae160a97008f7331510b77f
Kieran Kunhya <kierank@ob-encoder.com>
2015-07-07 15:58:47 +0100

    Set opaque pointer size correctly

--- lib/upipe-av/upipe_avcodec_decode.c
+++ lib/upipe-av/upipe_avcodec_decode.c
@@ -861,7 +861,7 @@ static void upipe_avcdec_output_pic(struct upipe *upipe, struct upump **upump_p)
 
     side_data = av_frame_get_side_data(frame, AV_FRAME_DATA_A53_CC);
     if (side_data)
-        uref_pic_set_cea_708(uref, side_data->data);
+        uref_pic_set_cea_708(uref, side_data->data, side_data->size);
 
     /* various time-related attributes */
     upipe_avcdec_set_time_attributes(upipe, uref);
