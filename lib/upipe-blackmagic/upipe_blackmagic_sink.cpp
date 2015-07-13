827071dd192161ffa88a79692ad600758d822b2b
Kieran Kunhya <kierank@obe.tv>
2015-07-13 16:55:08 +0100

    Make 16 channels of audio the default

--- lib/upipe-blackmagic/upipe_blackmagic_sink.cpp
+++ lib/upipe-blackmagic/upipe_blackmagic_sink.cpp
@@ -729,7 +729,7 @@ static int upipe_bmd_sink_set_uri(struct upipe *upipe, const char *uri)
         goto end;
     }
 
-    result = deckLinkOutput->EnableAudioOutput(48000, bmdAudioSampleType32bitInteger, 2, bmdAudioOutputStreamTimestamped);
+    result = deckLinkOutput->EnableAudioOutput(48000, bmdAudioSampleType32bitInteger, 16, bmdAudioOutputStreamTimestamped);
     if (result != S_OK)
     {
         fprintf(stderr, "Failed to enable audio output. Is another application using the card?\n");
