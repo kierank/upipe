fe717b872dfe4a7b0c3b1c9f7071e8fa60314413
James Darnley <james.darnley@gmail.com>
2017-08-24 12:32:01 +0200

    add forgotten header

---
+++ include/upipe-modules/upipe_rtp_vc2_pack.h
@@ -0,0 +1,15 @@
+#ifndef _UPIPE_MODULES_RTP_VC2_PACK_H_
+# define _UPIPE_MODULES_RTP_VC2_PACK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define UPIPE_RTP_VC2_PACK_SIGNATURE UBASE_FOURCC('r','t','p','v')
+struct upipe_mgr *upipe_rtp_vc2_pack_mgr_alloc(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
