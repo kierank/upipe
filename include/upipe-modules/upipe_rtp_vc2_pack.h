385419b2cb130f3a82a6935d09c1cf1dd761603f
James Darnley <james.darnley@gmail.com>
2017-08-28 15:46:53 +0200

    rtp-vc2-pack: change fourCC to allow an unpacker to have a similar code

--- include/upipe-modules/upipe_rtp_vc2_pack.h
+++ include/upipe-modules/upipe_rtp_vc2_pack.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-#define UPIPE_RTP_VC2_PACK_SIGNATURE UBASE_FOURCC('r','t','p','v')
+#define UPIPE_RTP_VC2_PACK_SIGNATURE UBASE_FOURCC('r','v','2','e')
 struct upipe_mgr *upipe_rtp_vc2_pack_mgr_alloc(void);
 
 #ifdef __cplusplus
