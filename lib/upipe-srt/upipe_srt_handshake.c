633185b3539f7a05dbd70ca097d72bcfad902832
Kieran Kunhya <kierank@obe.tv>
2023-11-01 22:26:02 +0000

    srth: Match 250ms timeout of libsrt
    Fixes issues on SRT endpoints with long ping times (e.g London to Mumbai)

--- lib/upipe-srt/upipe_srt_handshake.c
+++ lib/upipe-srt/upipe_srt_handshake.c
@@ -380,7 +380,7 @@ static void upipe_srt_handshake_timer(struct upump *upump)
 
     uint64_t now = uclock_now(upipe_srt_handshake->uclock);
 
-    if (now - upipe_srt_handshake->last_hs_sent < UCLOCK_FREQ / 10) {
+    if (now - upipe_srt_handshake->last_hs_sent < UCLOCK_FREQ / 4) {
         return;
     }
 
